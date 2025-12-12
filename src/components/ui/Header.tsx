9df222781a6c9db3415e89d83ac9675a537c12c1
Emre Gundogdu <emreguendogdu@yandex.com>
2025-12-12 21:26:25 +0300

    fix: Button descendant error

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useCartContext } from "@/context/CartContext";
-import Link from "next/link";
-import { CartIcon } from "@/components/ui/Icons";
 import { Product } from "@/types/product";
 import MenuButton from "./MenuButton";
 import Logo from "./Logo";
@@ -25,16 +23,15 @@ export default function Header({ products }: HeaderProps) {
       <header className="fixed top-0 left-0 right-0 flex items-center justify-between py-4 bg-black text-neutral-500 z-50 px-sectionX-m md:px-sectionX">
         <Logo />
         <div className="flex items-center gap-8">
+          {/* Cart Button */}
           <button
-            className="flex gap-2 items-center group cursor-pointer focus:outline-none"
+            className="flex gap-2 items-center group cursor-pointer focus:outline-none subheading text-neutral-500 group-hover:text-neutral-200 transition-all border border-neutral-500 px-4 py-1 rounded-full"
             onClick={() => setDisplayCart(true)}
           >
-            <span className="subheading text-neutral-500 group-hover:text-neutral-200 transition-all border border-neutral-500 px-4 py-1 rounded-full">
-              CART ({cart?.cartItems?.length || 0})
-            </span>
-
-            <MenuButton products={products} />
+            CART ({cart?.cartItems?.length || 0})
           </button>
+
+          <MenuButton products={products} />
         </div>
       </header>
     </>
